2276ca1d831bd52383b6b68d5d6242069af404da
Fajar Fatima <fatimafajar231@gmail.com>
2025-10-21 18:51:49 +0500

    Update package.java

---
+++ package.java
@@ -0,0 +1,7 @@
+/*
+Package is a group of similar types of classes, interfaces and sub-packages. Packages can be built-in or user defined.
+Built-in packages - java, util, io etc. */
+
+import java.util.Scanner;
+
+import java.io.IOException;
